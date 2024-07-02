2fc70c825b23588b0c280bccece5fc459cecaba5
Misha Klyukin <klumisha@gmail.com>
2024-07-02 19:00:00 +0300

    setup logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"log/slog"
 
 	"github.com/MaKYaro/url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
 	// TODO: parse config
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
+	log.Info("logger is working", slog.String("env", cfg.Env))
+	log.Debug("debug masseges are enabled")
 
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug},
+		),
+		)
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelInfo},
+		),
+		)
+	}
+	return log
 }
