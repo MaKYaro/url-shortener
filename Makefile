7f64b3b4ae71f0683ebf848d0acbf37597f097c4
Misha Klyukin <klumisha@gmail.com>
2024-07-20 14:49:43 +0300

    implemented graceful shutdown for server

--- Makefile
+++ Makefile
@@ -7,3 +7,23 @@ cover:
 .PHONY: clean-cover
 clean-cover:
 	rm c.out coverage.html
+
+.PHONY: start-postgres
+start-postgres:
+	sudo service postgresql start
+
+.PHONY: stop-postgres
+stop-postgres:
+	sudo service postgresql stop
+
+.PHONY: run-shortener
+run-shortener:
+	go run ./cmd/url-shortener/main.go --config-path=./config/local.json
+
+.PHONY: up-migrations
+up-migrations:
+	goose -dir ./migrations postgres "postgresql://postgres:postgres@localhost:5432/url_shortener?sslmode=disable" up
+
+.PHONY: down-migrations
+down-migrations:
+	goose -dir ./migrations postgres "postgresql://postgres:postgres@localhost:5432/url_shortener?sslmode=disable" down
--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"net/http"
 	"os"
-	"time"
+	"os/signal"
+	"syscall"
 
 	"log/slog"
 
 	"github.com/MaKYaro/url-shortener/internal/config"
-	"github.com/MaKYaro/url-shortener/internal/domain"
+	"github.com/MaKYaro/url-shortener/internal/http-server/handlers/url/save"
+	"github.com/MaKYaro/url-shortener/internal/lib/random"
+	urlshortener "github.com/MaKYaro/url-shortener/internal/services/url-shortener"
 	"github.com/MaKYaro/url-shortener/internal/storage/postgres"
 )
 
@@ -27,46 +31,61 @@ func main() {
 
 	storage, err := postgres.New(cfg.DBConn)
 	defer storage.Close()
+
 	if err != nil {
 		log.Error("can't init storage", slog.String("error", err.Error()))
 	}
-	err = storage.SaveURL(
-		&domain.Alias{
-			Value:  "goose",
-			URL:    "https://habr.com/ru/articles/780280/",
-			Expire: time.Now(),
-		},
+	log.Info("storage is working", slog.Any("storage", cfg.DBConn))
+
+	aliasGenerator := random.NewGenerator(cfg.Alias.Length)
+	shortener := urlshortener.New(
+		log,
+		storage,
+		storage,
+		storage,
+		aliasGenerator,
+		cfg.Alias.LifeLength,
 	)
-	if err != nil {
-		log.Error("can't save url", slog.String("error", err.Error()))
-	}
-	err = storage.SaveURL(
-		&domain.Alias{
-			Value:  "goose",
-			URL:    "https://habr.com/ru/articles/780280/",
-			Expire: time.Now(),
-		},
-	)
-	if err != nil {
-		log.Error("can't save url", slog.String("error", err.Error()))
-	}
-	alias, err := storage.GetURL("goose")
-	fmt.Println(alias)
-	if err != nil {
-		log.Error("can't get url", slog.String("error", err.Error()))
-	}
-	_, err = storage.GetURL("goo")
-	if err != nil {
-		log.Error("can't get url", slog.String("error", err.Error()))
-	}
-	err = storage.DeleteURL("adfadf")
-	if err != nil {
-		log.Error("can't delete url", slog.String("error", err.Error()))
+
+	router := http.NewServeMux()
+	router.HandleFunc("POST /url", save.New(log, shortener))
+
+	log.Info("starting server", slog.String("address", cfg.Server.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Server.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Server.Timeout,
+		WriteTimeout: cfg.Server.Timeout,
+		IdleTimeout:  cfg.Server.IdleTimeout,
 	}
 
-	// TODO: init router
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 
-	// TODO: run app
+		sign := <-stop
+
+		log.Info(
+			"stopping application",
+			slog.String("signal", sign.String()),
+		)
+
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Error(
+				"failed to shutdown server",
+				slog.String("error", err.Error()),
+			)
+		}
+		close(stop)
+	}()
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Info("server stopped")
+	log.Info("application stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
